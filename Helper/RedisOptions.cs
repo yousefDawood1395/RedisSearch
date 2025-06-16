a26af40980bc9fda939931f9b7bb0a10a56e2142
yadawood <yadawood@mc.gov.sa>
2025-06-16 11:53:46 +0300

    add redis oprions configurations

---
+++ Helper/RedisOptions.cs
@@ -0,0 +1,11 @@
+﻿using StackExchange.Redis.MultiplexerPool;
+
+namespace RedisSearch.Helper
+{
+    public class RedisOptions
+    {
+        public string ConnectionString { get; set; } = "";
+        public int PoolSize { get; set; } = Environment.ProcessorCount;
+        public ConnectionSelectionStrategy Strategy { get; set; } = ConnectionSelectionStrategy.LeastLoaded;
+    }
+}
